febc4943feb79514333060e03130b775a90a6ecf
Ramachandra-2k96 <ramachandraudupa.sangamone@gmail.com>
2025-09-11 14:28:31 +0530

    feat: Update MEDIA_ROOT configuration and add new media file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = "static/"
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
